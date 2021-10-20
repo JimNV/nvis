7d93f63b0885ebc5fda751ee1a8438946f664b4f
Jim Nilsson <jim@benolin.se>
2021-10-20 08:33:03 +0200

    Renamed bash http-server script, added .bat

---
+++ bin/nvis.bat
@@ -0,0 +1,11 @@
+@echo off
+set PORT=80
+set SCRIPT_DIR=%~dp0
+set SCRIPT_DIR=%SCRIPT_DIR:~0,-1%
+for /D %%D in ("%SCRIPT_DIR%") do (
+    set "NVIS_DIR=%%~dpD"
+)
+
+rem #PYTHON_VERSION=`python --version`
+rem echo "Starting HTTP server on port ${PORT} in ${SCRIPT_DIR}"
+python -m http.server --bind localhost %PORT% -d %NVIS_DIR%
--- bin/nvis.sh
+++ bin/nvis.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 PORT=80
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
-SCRIPT_DIR=${SCRIPT_DIR%/*}
+NVIS_DIR=${SCRIPT_DIR%/*}
+
+#PYTHON_VERSION=`python --version`
 echo "Starting HTTP server on port ${PORT} in ${SCRIPT_DIR}"
-python -m http.server --bind localhost ${PORT} -d ${SCRIPT_DIR}/
+python -m http.server --bind localhost ${PORT} -d ${NVIS_DIR}/
